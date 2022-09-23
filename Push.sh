45886134f5f50812fec0584c7d1b0c26ee0a89eb
Alireza-Kazemi <alireza.kzmi@gmail.com>
2022-09-23 04:40:33 +0000

    New

--- Push.sh
+++ Push.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/kazemi/Documents/Github/BrainJamz/
 git add --all
 git commit -m "New"
 sh -c "git push https://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@github.com/Alireza-Kazemi/BrainJamz.git"
-cd ~
