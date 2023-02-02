00bedabb2438929e16340e2b267f91983f86505a
Alireza-Kazemi <alireza.kzmi@gmail.com>
2023-02-02 21:15:09 +0000

    ServerCommit

--- Push.sh
+++ Push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add --all
 git commit -m "ServerCommit"
-sh -c "git push https://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@github.com/Alireza-Kazemi/BrainJamz.git"
+sh -c "git push https://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@github.com/Alireza-Kazemi/BrainJamz.git"
 
